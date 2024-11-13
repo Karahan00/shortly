11b0b827a7c433c9f7dda72a70988715f1aac9fa
Karahan Osman <karahan.osman@epppuls.com>
2024-11-13 07:00:35 +0100

    Implementation of Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shortly/Controller/Home/HomeController.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-import 'Config/Constants.dart';
+void main() async {
 
-void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     const ProviderScope(
       child: ShortlyApp(),
